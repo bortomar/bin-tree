6b7fcfd5f22c7179f657580e0e8c0371fd089689
root <root@whatever>
2023-02-15 01:34:02 +0100

    typescript

--- src/components/TreeNode.vue
+++ src/components/TreeNode.vue
@@ -2,17 +2,17 @@
     <line v-if="node?.left" :x1="node.x" :y1="node.y" :x2="node?.left.x" :y2="node.left.y" stroke="black" />
     <line v-if="node?.right" :x1="node.x" :y1="node.y" :x2="node?.right.x" :y2="node.right.y" stroke="black" />
     <circle :cx="node.x" :cy="node.y" :r="radius" fill="#00FFFF" stroke="black" />
-    <foreignObject :x="node.x - 25" :y="node.y+5" width="25" height="25">
+    <foreignObject :x="node.x! - 25" :y="node.y!+5" width="25" height="25">
         <q-btn flat v-if="!node.left"  dense @click="$emit('add-left', node)" label="+"/>
         <q-btn flat v-else  dense @click="$emit('remove-left', node)" label="-"/>
     </foreignObject>
-    <foreignObject :x="node.x+10" :y="node.y+5" width="25" height="25">
+    <foreignObject :x="node.x!+10" :y="node.y!+5" width="25" height="25">
         <q-btn flat v-if="!node.right"  dense @click="$emit('add-right', node)" label="+"/>
         <q-btn flat v-else  dense @click="$emit('remove-right', node)" label="-"/>
     </foreignObject>
     <text 
-    	:x="node.x-5" 
-      :y="node.y+4" 
+    	:x="node.x!-5" 
+      :y="node.y!+4" 
       style="font-size: 10px"
      >
      	{{ node.value }}
